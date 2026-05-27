575d328eb910079cf672532dd5b1d3a42bc509d4
jianxing86 <jxchen_cn@outlook.com>
2026-05-27 14:24:06 +0800

    feat: update quotation marks for consistency in site configuration and content files

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,6 +1,6 @@
 export const SITE_CONFIG = {
   author: { en: 'Jianxing Chen', zh: '陈剑星' },
-  position: { en: '"Liyun" Postdoctoral Fellow', zh: '励耘博士后' },
+  position: { en: '“Liyun” Postdoctoral Fellow', zh: '励耘博士后' },
   affiliation: { en: 'School of Physics and Astronomy, Beijing Normal University', zh: '北京师范大学物理与天文学院' },
   selfPattern: 'Chen, J.',
   social: {
